b745e02059972baab35e39946efeb834bb17ea1a
Kevin Ngai <kevin.ngai@canada.ca>
2021-09-27 10:19:56 -0700

    Update hydrometric test case for new HYDAT ES 2021-07-21

--- tests/e2e/specs/hydrometric.js
+++ tests/e2e/specs/hydrometric.js
@@ -64,7 +64,7 @@ describe('E2E test for hydrometric data with various form options', () => {
 
     // visit download link (limit 1)
     cy.get('#wfs3-link-list').scrollIntoView().wait(250).should('be.visible')
-    cy.get('#wfs3-link-list').find('a').should('have.lengthOf', 427)
+    cy.get('#wfs3-link-list').find('a').should('have.lengthOf', 428)
     cy.get('#wfs3-link-list a:first').should('have.attr', 'href').then((href) => {
       let hrefLimited = href.replace(/limit=\d+/, 'limit=1')
       cy.request('GET', hrefLimited).then((response) => {
