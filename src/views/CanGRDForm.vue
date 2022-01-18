b2abb861c7c3e1c7c2bc294d01d8c318edf7f8ac
Kevin Ngai <kevin.ngai@canada.ca>
2022-01-18 08:07:52 -0800

    Download region text update

--- src/views/CanGRDForm.vue
+++ src/views/CanGRDForm.vue
@@ -20,7 +20,9 @@
     <bbox-map
       v-model="ows_bbox"
       :allow-click-point="true"
+      :file-formats="fileFormats"
       @change="splitBBOXString"></bbox-map>
+
     <var-select
       v-model="oapicValueType"
       :label="$gettext('Value type')"
