d3df0f2607dd9b913f7c39cb7c1150bfa344c0a9
Kevin Ngai <kevin.ngai@canada.ca>
2022-02-03 14:20:16 -0800

    CanSIPS E2E test case updated for oapiCov

--- tests/e2e/specs/cansips.js
+++ tests/e2e/specs/cansips.js
@@ -1,16 +1,16 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('E2E test for CanSIPS WCS data with various form options', () => {
-  it('Test form changes and download sea level pressure data as GeoTIFF', () => {
+  it('Test form changes and download sea level pressure data as CoverageJSON', () => {
     cy.visit('/#/seasonal-forecasts')
 
     let varName = 'Sea level pressure'
-    let varVal = 'ETA_PN-SLP'
+    let varVal = '4'
     let member = '8'
     let modelDate = '2018-03'
     let forecastDate = '2018-04'
-    let formatName = 'GeoTIFF'
-    let formatVal = 'image/tiff'
+    let formatName = 'CoverageJSON'
+    let formatVal = 'json'
 
     // model type is disabled
     cy.get('#var-sel-model-type').scrollIntoView().should('be.disabled')
@@ -33,13 +33,13 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.get('a#download-url').should('have.attr', 'href').then((href) => {
       cy.request('GET', href).then((response) => {
         expect(response.status).to.equal(200)
-        // attachment; filename="geomet-weather_CANSIPS.MEM.ETA_PN-SLP.08_2018-09-01T0000Z_PT720H.tif"
-        expect(response.headers['content-disposition']).to.match(/.*geomet-weather_CANSIPS\.MEM\.ETA_PN-SLP\.08.*2018-03.*tif.*$/)
+        expect(response.body.type).to.equal('Coverage')
+        expect(response.headers['content-type']).to.equal('application/prs.coverage+json')
       })
     })
   })
 
-  it('Download air temperature data as GeoTIFF', () => {
+  it('Download air temperature data as CoverageJSON', () => {
     // Reset map
     cy.get('#reset-map-view').scrollIntoView().wait(250).click()
 
@@ -48,12 +48,12 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.wait(500) // mimic user pause after a zoom click
 
     let varName = 'Air temperature'
-    let varVal = 'ETA_TT'
+    let varVal = '1'
     let member = '2'
     let modelDate = '2018-06'
     let forecastDate = '2018-07'
-    let formatName = 'GeoTIFF'
-    let formatVal = 'image/tiff'
+    let formatName = 'CoverageJSON'
+    let formatVal = 'json'
 
     // model type is disabled
     cy.get('#var-sel-model-type').scrollIntoView().should('be.disabled')
@@ -76,12 +76,13 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.get('a#download-url').should('have.attr', 'href').then((href) => {
       cy.request('GET', href).then((response) => {
         expect(response.status).to.equal(200)
-        expect(response.headers['content-disposition']).to.match(/.*geomet-weather_CANSIPS\.MEM\.ETA_TT\.02.*2018-06.*tif.*$/)
+        expect(response.body.type).to.equal('Coverage')
+        expect(response.headers['content-type']).to.equal('application/prs.coverage+json')
       })
     })
   })
 
-  it('Download water temperature data as GeoTIFF', () => {
+  it('Download water temperature data as CoverageJSON', () => {
     // Reset map
     cy.get('#reset-map-view').scrollIntoView().wait(250).click()
 
@@ -92,12 +93,12 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.wait(500)
 
     let varName = 'Water temperature'
-    let varVal = 'ETA_WTMP'
+    let varVal = '6'
     let member = '5'
     let modelDate = '2017-11'
     let forecastDate = '2017-12'
-    let formatName = 'GeoTIFF'
-    let formatVal = 'image/tiff'
+    let formatName = 'CoverageJSON'
+    let formatVal = 'json'
 
     // model type is disabled
     cy.get('#var-sel-model-type').scrollIntoView().should('be.disabled')
@@ -120,22 +121,23 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.get('a#download-url').should('have.attr', 'href').then((href) => {
       cy.request('GET', href).then((response) => {
         expect(response.status).to.equal(200)
-        expect(response.headers['content-disposition']).to.match(/.*geomet-weather_CANSIPS\.MEM\.ETA_WTMP\.05.*2017-11.*tif.*$/)
+        expect(response.body.type).to.equal('Coverage')
+        expect(response.headers['content-type']).to.equal('application/prs.coverage+json')
       })
     })
   })
 
-  it('Download air temperature at 850mb data as NetCDF', () => {
+  it('Download air temperature at 850mb data as GRIB', () => {
     // Reset map
     cy.get('#reset-map-view').scrollIntoView().wait(250).click()
 
     let varName = 'Air temperature at 850mb'
-    let varVal = 'PRES_TT.850'
+    let varVal = '5'
     let member = '13'
     let modelDate = '2013-05'
     let forecastDate = '2013-06'
-    let formatName = 'NetCDF'
-    let formatVal = 'image/netcdf'
+    let formatName = 'GRIB'
+    let formatVal = 'GRIB'
 
     // model type is disabled
     cy.get('#var-sel-model-type').scrollIntoView().should('be.disabled')
@@ -158,12 +160,12 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.get('a#download-url').should('have.attr', 'href').then((href) => {
       cy.request('GET', href).then((response) => {
         expect(response.status).to.equal(200)
-        expect(response.headers['content-disposition']).to.match(/.*geomet-weather_CANSIPS\.MEM\.PRES_TT\.850\.13.*2013-05.*nc.*$/)
+        expect(response.headers['content-disposition']).to.match(/.*cansips_forecast_raw_latlon2\.5x2\.5_TMP_ISBL_0850_2013-05_allmembers\.grib2.*$/)
       })
     })
   })
 
-  it('Download Instantaneous precipitation rate (m/s) data as NetCDF', () => {
+  it('Download Instantaneous precipitation rate (m/s) data as GRIB', () => {
     // Reset map
     cy.get('#reset-map-view').scrollIntoView().wait(250).click()
 
@@ -174,12 +176,12 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.wait(500)
 
     let varName = 'Instantaneous precipitation rate (m/s)'
-    let varVal = 'ETA_RT'
+    let varVal = '3'
     let member = '10'
     let modelDate = '2016-08'
     let forecastDate = '2016-09'
-    let formatName = 'NetCDF'
-    let formatVal = 'image/netcdf'
+    let formatName = 'GRIB'
+    let formatVal = 'GRIB'
 
     // model type is disabled
     cy.get('#var-sel-model-type').scrollIntoView().should('be.disabled')
@@ -202,7 +204,7 @@ describe('E2E test for CanSIPS WCS data with various form options', () => {
     cy.get('a#download-url').should('have.attr', 'href').then((href) => {
       cy.request('GET', href).then((response) => {
         expect(response.status).to.equal(200)
-        expect(response.headers['content-disposition']).to.match(/.*geomet-weather_CANSIPS\.MEM\.ETA_RT\.10.*2016-08.*nc.*$/)
+        expect(response.headers['content-disposition']).to.match(/.*cansips_forecast_raw_latlon2\.5x2\.5_PRATE_SFC_0_2016-08_allmembers\.grib2.*$/)
       })
     })
   })
