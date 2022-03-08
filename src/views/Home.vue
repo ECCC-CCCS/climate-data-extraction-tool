48cf6f64629be0a18709218a2a762d019fc16555
Kevin Ngai <kevin.ngai@canada.ca>
2022-03-08 09:18:55 -0800

    Added vue-carousel for dataset picture previews on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,33 @@
 
     <latest-release></latest-release>
 
+    <carousel :perPage="1" :autoplay="true" :loop="true" role="img" aria-live="polite">
+      <slide>
+        <img src="../assets/carousel/ExtAHCCD.png" class="img-responsive center" alt="Carousel image preview: AHCCD station point on map" />
+      </slide>
+      <slide>
+        <img src="../assets/carousel/ExtCanGrd.png" class="img-responsive center" alt="Carousel image preview: CanGrd intro" />
+      </slide>
+      <slide>
+        <img src="../assets/carousel/ExtCMIP.png" class="img-responsive center" alt="Carousel image preview: CMIP5 map options"/>
+      </slide>
+      <slide>
+        <img src="../assets/carousel/ExtDaily.png" class="img-responsive center" alt="Carousel image preview: Daily station point on map" />
+      </slide>
+      <slide>
+        <img src="../assets/carousel/ExtDCSOptions.png" class="img-responsive center" alt="Carousel image preview: DCS page options" />
+      </slide>
+      <slide>
+        <img src="../assets/carousel/ExtHourly.png" class="img-responsive center" alt="Carousel image preview: Hourly station point on map" />
+      </slide>
+      <slide>
+        <img src="../assets/carousel/ExtLTCE.png" class="img-responsive center" alt="Carousel image preview: LTCE station point on map"/>
+      </slide>
+      <slide>
+        <img src="../assets/carousel/ExtNorm.png" class="img-responsive center" alt="Carousel image preview: Normals station point on map" />
+      </slide>
+    </carousel>
+
     <p v-translate>Use the climate data extraction tool to download climate data from the selected Environment and Climate Change Canada's datasets. You can specify the date ranges, variables, download format and other options.</p>
 
     <strong v-translate>Get started</strong>
@@ -19,7 +46,7 @@
 </template>
 
 <script>
-
+import { Carousel, Slide } from 'vue-carousel'
 import MoreResources from '@/components/MoreResources'
 import LatestRelease from '@/components/LatestRelease'
 import { ows } from '@/components/mixins/ows'
@@ -30,11 +57,20 @@ export default {
   mixins: [ows, datasets],
   components: {
     MoreResources,
-    LatestRelease
+    LatestRelease,
+    Carousel,
+    Slide
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.center {
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
 </style>
