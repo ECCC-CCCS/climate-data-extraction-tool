cac15ec424b16bbfbe78594d919ae19a65545949
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-14 14:09:46 +0800

    Delete build directory

--- build/gettext-compile.js
+++
@@ -1,7 +0,0 @@
-const { Compiler } = require('vue-gettext-tools')
-const gettextOptions = require('./config/i18n')
-
-const compilerConfig = {verbose: true}
-const locales = gettextOptions.allLocales
-
-Compiler(compilerConfig, 'locales/po/dictionary.pot', 'locales/po/', 'locales/json/', locales)
--- build/gettext-extract.js
+++
@@ -1,5 +0,0 @@
-const { Extractor } = require('vue-gettext-tools')
-
-const extractorConfig = {verbose: true}
-
-Extractor(extractorConfig, ['src/**/*.{vue,js}'], 'locales/po/dictionary.pot')
