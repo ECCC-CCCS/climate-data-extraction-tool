474e5b334dea67228ff73ff55e4999d4a7bb68a3
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-14 14:12:07 +0800

    Delete tests/e2e directory

--- tests/e2e/specs/404.js
+++
@@ -1,9 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('E2E test for 404 page', () => {
-  it('Visits a non-existent page and get 404 page', () => {
-    cy.visit('/#/a-page-that-does-not-exist')
-    cy.contains('We couldn\'t find that Web page (Error 404)').should('be.visible')
-    cy.contains('Nous ne pouvons trouver cette page Web (Erreur 404)').should('be.visible')
-  })
-})
