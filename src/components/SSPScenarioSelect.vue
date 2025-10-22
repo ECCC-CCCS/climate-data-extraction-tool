3cc614c929be2e4ad0e31288dc7af1759db5f0a3
Justin Tran <jstntn00@gmail.com>
2025-10-22 15:39:21 -0400

    CanDCSU6 data download page addition issue-1200

---
+++ src/components/SSPScenarioSelect.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="form-group">
+    <label
+      for="ssp_scenario"
+      :class="{required: required}">
+      <translate>Emissions scenario</translate>
+      <strong
+        v-if="required"
+        class="required"
+        aria-required="true"
+        v-translate>(Required)</strong>
+      <br>
+    </label>
+    <select class="form-control" id="ssp_scenario"
+      :value="value"
+      @change="emitUpdatedValue">
+        <option
+          v-for="(scenarioValue, key) in selectOptions"
+          :key="key"
+          :value="key"
+          v-html="scenarioValue"
+          :required="required"></option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScenarioSelect',
+  props: {
+    value: String,
+    selectOptions: {
+      type: Object,
+      default: function () {
+        return {
+          'null': 'N/A' // this.$gettext('No options available')
+        }
+      }
+    },
+    required: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    emitUpdatedValue: function (event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
